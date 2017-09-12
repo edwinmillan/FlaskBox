a41b49f0ad6ef6ff0d3e9dac0711b93d431ebeaa
Edwin Millan <bassgs3000@gmail.com>
2017-09-11 20:19:29 -0500

    Templating and css

--- flaskbox/flaskbox.py
+++ flaskbox/flaskbox.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,8 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello!"
+    return render_template('index.html',
+                           title="Index")
 
+@app.route('/tools')
+def tools():
+    return render_template('tools.html',
+                           title="Tools")
 
 def main():
     app.run(host='0.0.0.0', debug=True)
