24eef797fc257c8aad8e2110ad3705f0d267ad5e
Edwin Millan <bassgs3000@gmail.com>
2017-09-11 20:48:07 -0500

    Added basic dns lookup forms

--- flaskbox/flaskbox.py
+++ flaskbox/flaskbox.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import *
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update({
+    'WTF_CSRF_ENABLED': True,
+    'SECRET_KEY': 'super-secret-change-me-you-knob'
+})
 
 
 @app.route('/')
@@ -10,10 +16,23 @@ def index():
     return render_template('index.html',
                            title="Index")
 
+
 @app.route('/tools')
 def tools():
     return render_template('tools.html',
                            title="Tools")
+
+
+@app.route('/tools/dns', methods=['GET', 'POST'])
+def dns_lookup():
+    form = DnsLookupForm()
+    if form.validate_on_submit():
+        flash(f'DNS Lookup for Hostname {form.hostname.data}')
+        return redirect('/tools')
+    return render_template('dns_lookup.html',
+                           title='Dns Lookup',
+                           form=form)
+
 
 def main():
     app.run(host='0.0.0.0', debug=True)
